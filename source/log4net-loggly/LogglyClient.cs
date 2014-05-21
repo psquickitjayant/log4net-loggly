34ac96441c86fac98a43e6ff4449d544851c580b
psquickitjayant <jayant.varshney@psquickit.com>
2014-05-21 17:52:03 +0530

    Code Formatting Updated

--- source/log4net-loggly/LogglyClient.cs
+++ source/log4net-loggly/LogglyClient.cs
@@ -9,10 +9,9 @@ namespace log4net.loggly
 		public virtual void Send(ILogglyAppenderConfig config, string inputKey,string userAgent, string message)
 		{
 			var bytes = Encoding.UTF8.GetBytes(message);
-            var request = CreateWebRequest(config, string.IsNullOrWhiteSpace(inputKey) ? config.InputKey : inputKey
-                , string.IsNullOrWhiteSpace(userAgent) ? config.UserAgent : userAgent);
+    			var request = CreateWebRequest(config, string.IsNullOrWhiteSpace(inputKey) ? config.InputKey : inputKey                , string.IsNullOrWhiteSpace(userAgent) ? config.UserAgent : userAgent);
 			
-            using (var dataStream = request.GetRequestStream())
+            		using (var dataStream = request.GetRequestStream())
 			{
 				dataStream.Write(bytes, 0, bytes.Length);
 				dataStream.Flush();
@@ -24,11 +23,11 @@ namespace log4net.loggly
 
 		protected virtual HttpWebRequest CreateWebRequest(ILogglyAppenderConfig config, string inputKey, string userAgent)
 		{
-            string tagInfo = userAgent;
+            		string tagInfo = userAgent;
 			var url = String.Concat(config.RootUrl, inputKey);
 
-            //adding userAgent as tag in the log
-            url = String.Concat(url, "/tag/" + userAgent);
+	                //adding userAgent as tag in the log
+	                url = String.Concat(url, "/tag/" + userAgent);
 			var request = (HttpWebRequest)WebRequest.Create(url);
 			request.Method = "POST";
 			request.ReadWriteTimeout = request.Timeout = config.TimeoutInSeconds * 1000;
